6cf86f1cea1064defe245bbd34f08d45efbf1094
Danilo Leocata <37438774+khalld@users.noreply.github.com>
2023-01-04 15:59:46 +0100

    Moved Test in appropriate package

--- src/test/java/com/isd/authentication/UsersAppTest.java
+++ src/test/java/com/isd/authentication/UsersAppTest.java
@@ -1,22 +1,12 @@
-package com.isd.authentication.test;
+package com.isd.authentication;
 
-import com.isd.authentication.controller.UserController;
-import com.isd.authentication.domain.User;
-import com.isd.authentication.dto.UserBalanceDTO;
-import com.isd.authentication.mapper.UserMapperService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
